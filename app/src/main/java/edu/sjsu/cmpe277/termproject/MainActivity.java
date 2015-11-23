b7f9371c6093f79a0c2855d7811156db51076884
eitegbe <eitegbe@gmail.com>
2015-11-23 03:42:15 -0800

    Facebook login

--- app/src/main/java/edu/sjsu/cmpe277/termproject/MainActivity.java
+++ app/src/main/java/edu/sjsu/cmpe277/termproject/MainActivity.java
@@ -1,24 +1,45 @@
 package edu.sjsu.cmpe277.termproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.Arrays;
+
+import edu.sjsu.cmpe277.termproject.models.User;
 
 public class MainActivity extends AppCompatActivity {
 
     private CallbackManager callbackManager;
     private LoginButton loginButton;
+    private User user;
+    private Intent intent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //initialize facebook sdk
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+
+        FaceBookSetup();
     }
 
     @Override
@@ -39,7 +60,56 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    private void FaceBookSetup() {
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions(Arrays.asList("public_profile"));
+
+       loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+           @Override
+           public void onSuccess(LoginResult loginResult) {
+               GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                   @Override
+                   public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+                       try {
+                            user = new User();
+                            user.setFirstName(jsonObject.getString("first_name"));
+                            user.setLastName(jsonObject.getString("last_name"));
+                            Log.d("VISUAL: ", user.getFirstName());
+                        }
+                        catch(JSONException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "first_name,last_name");
+                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+
+           }
+
+           @Override
+           public void onCancel() {
+
+           }
+
+           @Override
+           public void onError(FacebookException e) {
+
+           }
+       });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
