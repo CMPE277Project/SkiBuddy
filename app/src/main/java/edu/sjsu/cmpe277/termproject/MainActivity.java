69e35444decde7d1f673874b4ccefe16a8bcd392
eitegbe <eitegbe@gmail.com>
2015-11-22 19:45:21 -0800

    able to display login on screen

--- app/src/main/java/edu/sjsu/cmpe277/termproject/MainActivity.java
+++ app/src/main/java/edu/sjsu/cmpe277/termproject/MainActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.FacebookSdk;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
     }
 
