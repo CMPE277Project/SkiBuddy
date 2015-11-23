0a92cfbd3cb1b1efc26c7d7c542e5cd45939e55b
eitegbe <eitegbe@gmail.com>
2015-11-22 17:52:41 -0800

    first commit

---
+++ app/src/main/java/edu/sjsu/cmpe277/termproject/models/User.java
@@ -0,0 +1,35 @@
+package edu.sjsu.cmpe277.termproject.models;
+
+/**
+ * Created by emy on 11/22/15.
+ */
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String imageFile;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(String imageFile) {
+        this.imageFile = imageFile;
+    }
+}
